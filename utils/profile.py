0483f462c6870c0fb0738de71e2f029af42d7dfe
marco cammarata <marcocamma@gmail.com>
2019-02-28 22:14:10 +0100

    better profile analysis functions

--- utils/profile.py
+++ utils/profile.py
@@ -1,5 +1,6 @@
 import numpy as np
 from matplotlib import pyplot as plt
+from datastorage import DataStorage
 
 def fit_1d(x,y,model="gaussian"):
     import lmfit
@@ -15,25 +16,33 @@ def fit_1d(x,y,model="gaussian"):
     return out
 
 
-def analize_2dprofile(x,y,img,model="gaussian"):
-    fig=plt.figure()
-    ax0 = fig.add_axes( [0.1,0.1,0.5,0.5] )
-    ax0.pcolormesh(x,y,img)
-    def fit(x,y):
-        out = fit_1d(x,y,model=model)
+def analyze_2dprofile(x,y,img,model="gaussian",plot=True):
+
+    if x.ndim == 2: x=x[:,0]
+    if y.ndim == 2: y=y[0]
+
+    def fit(x,intensity):
+        out = fit_1d(x,intensity,model=model)
         print(out.params)
-        return out.best_fit,out.params["fwhm"].value
+        return out,out.best_fit,out.params["fwhm"].value
 
-    ax = fig.add_axes( [0.6,0.1,0.3,0.5],sharey=ax0 )
-    ax.plot( img.mean(0),y.mean(0) )
-    f1,fwhm1 = fit( y.mean(0),img.mean(0) )
-    ax.plot( f1,y.mean(0) )
-    ax.set_title("fwhm = %.1f" % fwhm1)
+    ox,fx,fwhmx = fit( x,img.mean(1) )
+    oy,fy,fwhmy = fit( y,img.mean(0) )
 
-    ax = fig.add_axes( [0.1,0.6,0.5,0.3],sharex=ax0 )
-    ax.plot( x.mean(1),img.mean(1) )
-    f2,fwhm2 = fit( x.mean(1),img.mean(1) )
-    ax.plot( x.mean(1),f2 )
-    ax.set_title("fwhm = %.1f" % fwhm2)
-    return 
+    if plot:
+        fig=plt.figure()
+        ax0 = fig.add_axes( [0.1,0.1,0.5,0.5] )
+        ax0.pcolormesh(x,y,img)
+
+        ax = fig.add_axes( [0.6,0.1,0.3,0.5],sharey=ax0 )
+        ax.plot( img.mean(0),y )
+        ax.plot( fy,y )
+        ax.set_title("fwhm = %.1f" % fwhmy)
+
+        ax = fig.add_axes( [0.1,0.6,0.5,0.3],sharex=ax0 )
+        ax.plot( x ,img.mean(1) )
+        ax.plot( x,fx )
+        ax.set_title("fwhm = %.1f" % fwhmx)
+    out = DataStorage( x=x,y=y,data=img,fitx=ox,fity=oy)
+    return out
 
