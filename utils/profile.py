f4bdcaf4f09ddffa8e1b845bfd69291dd057fc18
Marco Cammarata <marco.cammarata@univ-rennes1.fr>
2019-03-01 23:18:25 +0100

    new function for fit beam size by knowing tranmission trough variable openin

--- utils/profile.py
+++ utils/profile.py
@@ -26,8 +26,11 @@ def analyze_2dprofile(x,y,img,model="gaussian",plot=True):
         print(out.params)
         return out,out.best_fit,out.params["fwhm"].value
 
-    ox,fx,fwhmx = fit( x,img.mean(1) )
-    oy,fy,fwhmy = fit( y,img.mean(0) )
+    projx = img.mean(0)
+    projy = img.mean(1)
+
+    ox,fx,fwhmx = fit( x,projx )
+    oy,fy,fwhmy = fit( y,projy )
 
     if plot:
         fig=plt.figure()
@@ -35,14 +38,32 @@ def analyze_2dprofile(x,y,img,model="gaussian",plot=True):
         ax0.pcolormesh(x,y,img)
 
         ax = fig.add_axes( [0.6,0.1,0.3,0.5],sharey=ax0 )
-        ax.plot( img.mean(0),y )
+        ax.plot( projy,y )
         ax.plot( fy,y )
         ax.set_title("fwhm = %.1f" % fwhmy)
 
         ax = fig.add_axes( [0.1,0.6,0.5,0.3],sharex=ax0 )
-        ax.plot( x ,img.mean(1) )
+        ax.plot( x ,projx )
         ax.plot( x,fx )
         ax.set_title("fwhm = %.1f" % fwhmx)
     out = DataStorage( x=x,y=y,data=img,fitx=ox,fity=oy)
     return out
+
+def transmission_trough_pinhole(opening,intensity=1,beam_fwhm=1):
+    """ Transmission of a gaussian beam trough a pinhole of a given size """
+    sigma = beam_fwhm/2.35482004503 # sqrt(8*ln2)
+    return intensity*(1-np.exp(-opening**2/2/sigma**2))
+
+def fit_transmission_trough_pinhole(opening,intensity,fix_intensity=False,plot=True):
+    import lmfit
+    model = lmfit.Model(transmission_trough_pinhole)
+    pars = model.make_params( intensity=intensity.max(), beam_fwhm=np.mean(opening))
+    if fix_intensity: pars['intensity'].vary = False
+    res = model.fit(intensity,x=opening)
+    if plot:
+        res.plot()
+    return res
+
+
+
 
