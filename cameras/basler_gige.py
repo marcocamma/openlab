26a8f392a3a690772103f7e676d229001aa13cb7
Marco Cammarata <marco.cammarata@univ-rennes1.fr>
2019-04-15 16:48:53 +0200

    improved basler wrapper, and unfinished microscope gui

---
+++ cameras/basler_gige.py
@@ -0,0 +1,181 @@
+from pypylon import pylon
+import time
+import cv2
+
+def get_camera(ip=None,num=0):
+    """ if ip is not given, num is used the select the camera number """
+    factory = pylon.TlFactory.GetInstance()
+    if ip is None:
+        cameras = factory.EnumerateDevices()
+        camera = cameras[num]
+        camera_device = factory.CreateDevice(camera)
+        camera = pylon.InstantCamera(camera_device)
+        #camera = pylon.InstantCamera(factory.CreateFirstDevice())
+    else:
+        ptl = factory.CreateTl('BaslerGigE')
+        empty_camera_info = ptl.CreateDeviceInfo()
+        empty_camera_info.SetPropertyValue('IpAddress', ip)
+        camera_device = factory.CreateDevice(empty_camera_info)
+        camera = pylon.InstantCamera(camera_device)
+    try:
+        camera.Open()
+        print("Opening ",camera.DeviceModelName())
+    except:
+        pass
+    return camera
+
+class Camera(object):
+    def __init__(self,ip=None,num=0,strategy=None):
+        """ if ip is not given, num is used the select the camera number """
+        self.camera = get_camera(ip=ip,num=num)
+        if strategy is None: strategy = pylon.GrabStrategy_LatestImageOnly
+        self.strategy = strategy
+        #print(self.strategy)
+        self._get_info()
+
+    def set_gain(self,value):
+        self.camera.GainRaw.SetValue(value)
+
+    def get_gain(self):
+        return self.camera.GainRaw()
+
+    gain = property(get_gain,set_gain)
+
+    def set_exp_time(self,value):
+        value_us = int(value*1e6)
+        self.camera.ExposureTimeAbs.SetValue(value_us)
+
+    def get_exp_time(self):
+        value_us = self.camera.ExposureTimeAbs()
+        value = float(value_us/1e6)
+        return value
+    exp_time = property(get_exp_time,set_exp_time)
+
+    def get_format(self):
+        camera = self.camera
+        return camera.PixelFormat.GetValue()
+
+    def show_valid_format(self):
+        camera = self.camera
+        return camera.PixelFormat.Symbolics
+
+    def set_format(self,value):
+        if value not in valid_format:
+            ans = "Allowed pixel format for camera are %s"%str(valid_format)
+            raise ValueError(ans)
+        else:
+            camera.PixelFormat.SetValue(value)
+    format = property(get_format,set_format)
+
+    def get_image(self,timeout=5000,as_array=True):
+        image  = self.camera.GrabOne(timeout)
+        #image = converter.Convert(result)
+        if as_array:
+            image = image.GetArray()
+        return image
+
+    def get_images_old(self,timeout=5000):
+        camera = self.camera
+        n=0
+        t0=time.time()
+        if not camera.IsGrabbing():
+            camera.StartGrabbing(self.strategy)
+        try:
+            while camera.IsGrabbing():
+                image = camera.RetrieveResult(timeout)
+                if image.GrabSucceeded():
+                    # Access the image data
+                    #image = converter.Convert(grabResult)
+                    image = image.GetArrayZeroCopy()
+                    #image = image.GetArray()
+                    dt = time.time()-t0
+                    n += 1
+                    rate = n/dt
+                    print("rate",rate)
+                    yield image
+                    #print(image.sum())
+        except KeyboardInterrupt:
+            pass
+        finally:
+            camera.StopGrabbing()
+
+
+    def get_images(self,timeout=5000):
+        camera = self.camera
+        n=0
+        t0=time.time()
+        if not camera.IsGrabbing():
+            camera.StartGrabbing(self.strategy)
+        try:
+            while camera.IsGrabbing():
+                with camera.RetrieveResult(timeout) as result:
+                    with result.GetArrayZeroCopy() as image:
+                        dt = time.time()-t0
+                        n += 1
+                        rate = n/dt
+                        print("rate",rate)
+                        yield image
+        except KeyboardInterrupt:
+            pass
+        finally:
+            camera.StopGrabbing()
+
+    def _get_info(self):
+        camera = self.camera
+        info = camera.GetDeviceInfo()
+        ret = dict()
+        mne = info.GetUserDefinedName()
+        if mne != "": ret['name'] = mne
+        mac = info.GetMacAddress()
+        mac = ':'.join(mac[i:i+2] for i in range(0, len(mac), 2))
+        ret['mac'] = mac
+        ret['ip'] = info.GetIpAddress()
+        ret['model'] = info.GetModelName()
+        self.info = ret
+        return self.info
+
+    def show_info(self):
+        fields = "name model ip mac".split()
+        s = []
+        for f in fields:
+            if f in self.info:
+                s.append("%s = %s"%(f,self.info[f]))
+        s.append("format = %s"%self.get_format())
+        return "\n".join(s)
+
+    def get_str(self,full=False):
+        info = self.info
+        if "name" in info:
+            s = info["name"] + "(" + ret["model"] +")"
+        else:
+            s = info["model"]
+        if full:
+            s += ", ip %s" % self.info["ip"]
+            s += ", format %s" % self.format
+        return s
+
+
+    def __str__(self):
+        return self.get_str(full=True)
+
+    def __repr__(self):
+        name = self.info.get("name",self.info["model"])
+        return name+"("+self.info["ip"]+")"
+
+    def display(self):
+        camera = self.camera
+        while camera.IsGrabbing():
+            grabResult = camera.RetrieveResult(5000,
+                    pylon.TimeoutHandling_ThrowException)
+            if grabResult.GrabSucceeded():
+                # Access the image data
+                image = converter.Convert(grabResult)
+                img = image.GetArrayZeroCopy() #Array()
+                cv2.namedWindow('title', cv2.WINDOW_NORMAL)
+                cv2.imshow('title', img)
+                k = cv2.waitKey(1)
+                if k == 27:
+                    break
+            grabResult.Release()
+        camera.StopGrabbing()
+        cv2.destroyAllWindows()
---
+++ gui/microscope/gui_camera_mpl.py
@@ -0,0 +1,273 @@
+"""
+Simple GUI to display (and analyze spectrometer data)
+based on embedding_in_qt5_sgskip example
+"""
+import sys
+import time
+import matplotlib
+# Make sure that we are using QT5
+matplotlib.use('Qt5Agg')
+
+from PyQt5 import QtCore, QtWidgets
+from PyQt5.QtWidgets import QCheckBox,QLineEdit,QLabel
+
+
+import numpy as np
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+
+from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
+
+from matplotlib import pyplot
+
+import lmfit
+from matplotlib.figure import Figure
+
+class MakeUpCamera:
+    def __init__(self,shape=(1000,1000),integration_time=0.1,noise=2):
+        self.shape=shape
+        self.integration_time=integration_time
+        self.noise=noise
+        self._x = np.arange(shape[0])
+
+    def acquire(self,naverage=1):
+        img = np.zeros(self.shape)
+        for _ in range(naverage):
+            y = np.cos(self._x/100)*self.integration_time
+            img = [y for i in range(self.shape[1])]
+            img = np.asarray(img)
+            img += np.random.normal(scale=self.noise,size=self.shape)
+        return img/naverage
+
+    def set_integration_time(self,value):
+        self.integration_time = value
+
+def gaussfit(x,y):
+    g = lmfit.models.GaussianModel()
+    c = lmfit.models.ConstantModel()
+    pars_g = g.guess(y,x=x)
+    pars_c = c.guess(y[:5])
+    model = g+c
+    pars = pars_c+pars_g
+    print(pars)
+    res = model.fit(y,x=x,params=pars)
+    return res
+
+
+class MyDynamicMplCanvas(FigureCanvas):
+    """A canvas that updates itself every second with a new plot."""
+
+    def __init__(self, parent=None, width=5, height=4, dpi=100,
+            get_data=None,frame_rate=1):
+        fig = Figure(figsize=(width, height), dpi=dpi)
+        self.axes = fig.add_subplot(111)
+        self.axes.grid()
+        self.get_data=get_data
+
+        self.compute_initial_figure()
+
+        FigureCanvas.__init__(self, fig)
+        self.setParent(parent)
+
+        FigureCanvas.setSizePolicy(self,
+                                   QtWidgets.QSizePolicy.Expanding,
+                                   QtWidgets.QSizePolicy.Expanding)
+        FigureCanvas.updateGeometry(self)
+
+ 
+        timer = QtCore.QTimer(self)
+        timer.timeout.connect(self.update_figure)
+        wait_time = 1/frame_rate
+        wait_time_ms = wait_time*1e3
+        timer.start(wait_time_ms)
+
+    def compute_initial_figure(self):
+        intensities=self.get_data()
+        self.axes.imshow(self.get_data())
+
+    def update_figure(self):
+        self.axes.images[0].set_data(self.get_data())
+        self.draw()
+
+#    def plot_fit(self,res):
+#        x = res.userkws["x"]
+#        if len(self.axes.lines) == 1:
+#            self.axes.plot(x,res.best_fit)
+#        else:
+#            line = self.axes.lines[1]
+#            line.set_xdata(x)
+#            line.set_ydata(res.best_fit)
+            
+
+
+
+class ApplicationWindow(QtWidgets.QMainWindow):
+    def __init__(self,camera):
+        QtWidgets.QMainWindow.__init__(self)
+        self.setAttribute(QtCore.Qt.WA_DeleteOnClose)
+        self.setWindowTitle("application main window")
+
+        self.camera = camera
+
+        self.file_menu = QtWidgets.QMenu('&File', self)
+        self.file_menu.addAction('&Quit', self.fileQuit,
+                                 QtCore.Qt.CTRL + QtCore.Qt.Key_Q)
+        self.menuBar().addMenu(self.file_menu)
+
+        self.help_menu = QtWidgets.QMenu('&Help', self)
+        self.menuBar().addSeparator()
+        self.menuBar().addMenu(self.help_menu)
+
+        self.help_menu.addAction('&info', self.help)
+
+
+
+        self.normalize=False
+        self.subtract_dark = False
+        self.integration_time = 1
+        self.fit=False
+
+        self.main_widget = QtWidgets.QWidget(self)
+
+
+
+        input_pars_layout = QtWidgets.QHBoxLayout()
+        cbox = QCheckBox("Normlize",self)
+        cbox.stateChanged.connect(self.normalize_clickBox)
+        input_pars_layout.addWidget(cbox)
+
+        cbox = QCheckBox("Subtract Dark",self)
+        cbox.stateChanged.connect(self.subtract_dark_clickBox)
+        input_pars_layout.addWidget(cbox)
+
+        cbox = QCheckBox("Fit",self)
+        cbox.stateChanged.connect(self.fit_clickBox)
+        input_pars_layout.addWidget(cbox)
+
+        lab = QLabel("Integration time (s)",self)
+        input_pars_layout.addWidget(lab)
+
+        # Add text field
+        ledit = QLineEdit(self)
+        ledit.insert("0.1")
+        self.integration_time_lineedit = ledit
+        self.camera.set_integration_time(0.1)
+        input_pars_layout.addWidget(ledit)
+
+
+        lab = QLabel("number of spectra to average",self)
+        input_pars_layout.addWidget(lab)
+
+        # Add text field
+        ledit = QLineEdit(self)
+        ledit.insert("3")
+        self.naverage_lineedit = ledit
+        input_pars_layout.addWidget(ledit)
+
+
+
+
+        layout = QtWidgets.QVBoxLayout(self.main_widget)
+
+        dc = MyDynamicMplCanvas(self.main_widget, width=5, height=4, dpi=100,
+                get_data=self.get_data)
+
+        layout.addLayout(input_pars_layout)
+        layout.addWidget(dc)
+        self.plot_win = dc
+
+        self.info_line = QLineEdit(self)
+        self.info_line.insert("")
+        self.info_line.move(350,20)
+        self.info_line.resize(80,40)
+
+
+        self.navi_toolbar = NavigationToolbar(dc, self)
+        layout.addWidget(dc)  # the matplotlib canvas
+        layout.addWidget(self.navi_toolbar)
+        layout.addWidget(self.info_line)
+
+
+
+
+        self.main_widget.setFocus()
+        self.setCentralWidget(self.main_widget)
+
+        self.statusBar().showMessage("All hail matplotlib!", 2000)
+
+
+    def get_data(self):
+        try:
+            integration_time = self.integration_time_lineedit.text()
+            integration_time = float(integration_time)
+            if integration_time != self.integration_time:
+                self.camera.set_integration_time(integration_time)
+            self.integration_time = integration_time # store new value
+        except ValueError:
+            integration_time = self.integration_time
+
+        try:
+            nav = self.naverage_lineedit.text()
+            nav = int(nav)
+            self.naverage = nav
+        except ValueError:
+            nav = self.naverage
+
+        i = self.camera.acquire(naverage=nav)
+        if self.subtract_dark:
+            #bkg = (i[0:10].mean() + i[-10:].mean())/2
+            bkg = i[-10:].mean()
+            i -= bkg
+        if self.normalize:
+            i /= i.max()
+        if self.fit:
+            t0 = time.time()
+            wmin,wmax = self.plot_win.axes.get_xlim()
+            idx = (w>wmin)&(w<wmax)
+            wfit = w[idx]
+            ifit = i[idx]
+            res=gaussfit(wfit,ifit)
+            self.plot_win.plot_fit(res)
+            print(res.best_values.keys())
+            fwhm = res.best_values['sigma']*2.35
+            self.info_line.clear()
+            info = "area = %.2e, center = %.1f nm, fwhm bw = %.1f nm"%(
+                    res.best_values['amplitude'],
+                    res.best_values['center'],
+                    fwhm)
+
+            self.info_line.insert(info)
+            print(time.time()-t0)
+        return i
+
+    def fit_clickBox(self, state):
+        self.fit = QtCore.Qt.Checked
+
+    def normalize_clickBox(self, state):
+        self.normalize = QtCore.Qt.Checked
+
+    def subtract_dark_clickBox(self,state):
+        self.subtract_dark = QtCore.Qt.Checked
+
+    def fileQuit(self):
+        self.close()
+
+    def closeEvent(self, ce):
+        self.fileQuit()
+
+    def help(self):
+        QtWidgets.QMessageBox.about(self, "Help",
+                                    """read spectrometer data and oftionally perform fit on data defined by visile xaxis """
+                                )
+        
+
+if __name__ == "__main__":
+    try:
+        FAIL
+    except:
+        camera = MakeUpCamera()
+    qApp = QtWidgets.QApplication(sys.argv)
+    aw = ApplicationWindow(camera=camera)
+    aw.setWindowTitle("Camera simple GUI")
+    aw.show()
+    sys.exit(qApp.exec_())
+#qApp.exec_()
