8ffd2142d006b9c211e9c0009fda4faafb674dcf
marco cammarata <marcocamma@gmail.com>
2020-06-19 10:18:45 +0200

    import with try/except in case of missing modules

--- stages/__init__.py
+++ stages/__init__.py
@@ -1,2 +1,8 @@
-from .newport import newportESP
-from .zaber import ZaberStage
+try:
+    from .newport import newportESP
+except ImportError as e:
+    print("Could not import newportESP, error was: "+str(e))
+try:
+    from .zaber import ZaberStage
+except ImportError as e:
+    print("Could not import ZaberStage, error was: "+str(e))
